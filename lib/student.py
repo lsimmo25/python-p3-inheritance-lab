9cecf651c01e36f06bb7e6986edcb0a1a0a3aa6c
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-25 11:45:11 -0400

    lab complete

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name: str, last_name: str):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, knowledge: str):
+        self.knowledge.append(knowledge)
